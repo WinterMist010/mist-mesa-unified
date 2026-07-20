fee52e49ffb979d7168d384610398d4849f1d34c
WinterMist010 <85207868+WinterMist010@users.noreply.github.com>
2026-07-20 19:42:07 +0700

    Experiment: increase gmem cache sizes

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1390,9 +1390,9 @@ add_gpus([
             sysmem_ccu_depth_cache_fraction = CCUColorCacheFraction.THREE_QUARTER.value,
             sysmem_per_ccu_depth_cache_size = 32 * 1024,
             gmem_ccu_color_cache_fraction = CCUColorCacheFraction.EIGHTH.value,
-            gmem_per_ccu_color_cache_size = 16 * 1024,
+            gmem_per_ccu_color_cache_size = 32 * 1024,
             gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
-            gmem_per_ccu_depth_cache_size = 24 * 1024,
+            gmem_per_ccu_depth_cache_size = 32 * 1024,
             
             gmem_vpc_attr_buf_size = 16384,
             gmem_vpc_pos_buf_size = 12288,
