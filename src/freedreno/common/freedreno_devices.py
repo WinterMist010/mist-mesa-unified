b3d3db8d635c0b77d9f9ae03aa9e2731a2637ed1
WinterMist010 <85207868+WinterMist010@users.noreply.github.com>
2026-07-27 09:48:54 +0700

    Hak: reduce sysmem vpc increase sysmem per ccu color cache

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1377,11 +1377,11 @@ add_gpus([
     ], A6xxGPUInfo(
         CHIP.A8XX,
         [a7xx_base, a7xx_gen3, a8xx_base, a8xx_gen1, GPUProps(
-            sysmem_vpc_attr_buf_size = 262144, 
-            sysmem_vpc_pos_buf_size = 131072,
-            sysmem_vpc_bv_pos_buf_size = 65536,
+            sysmem_vpc_attr_buf_size = 131072, 
+            sysmem_vpc_pos_buf_size = 65536,
+            sysmem_vpc_bv_pos_buf_size = 32768,
             sysmem_ccu_color_cache_fraction = CCUColorCacheFraction.FULL.value,
-            sysmem_per_ccu_color_cache_size = 32 * 1024,
+            sysmem_per_ccu_color_cache_size = 128 * 1024,
             sysmem_ccu_depth_cache_fraction = CCUColorCacheFraction.THREE_QUARTER.value,
             sysmem_per_ccu_depth_cache_size = 32 * 1024,
             gmem_ccu_color_cache_fraction = CCUColorCacheFraction.EIGHTH.value,
