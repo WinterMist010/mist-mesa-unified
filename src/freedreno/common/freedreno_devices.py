7b2e806ae3e0b683807cec59ff375d5578525c58
WinterMist010 <85207868+WinterMist010@users.noreply.github.com>
2026-08-08 06:19:23 +0700

    num_ccu=2

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1478,7 +1478,7 @@ add_gpus([
             has_salu_int_narrowing_quirk = True,
             shading_rate_matches_vk = True,
         )],
-        num_ccu = 1,
+        num_ccu = 2,
         num_slices = 1,
         tile_align_w = 64,
         tile_align_h = 32,
